2cc7af1db343586083c616d21143c8ac03e2e859
sharoz <sherry@sharoz.dev>
2022-03-18 14:24:53 +0500

    Porfolio

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// const aboutPage = document.getElementById("aboutBTN");
+// const ProfilePic = document.querySelector(".profile__pic");
+
+// gsap.from(".animation-y", {
+//   duration: 1,
+//   y: 100,
+// });
+// gsap.from(".none", { duration: 1, x: 300, stagger: 0.23 });
+// gsap.from(".image__area", { duration: 1, x: 300 });
+// gsap.from(".icons", { duration: 1, x: 300 });
+
+// // aboutPage.addEventListener("click", () => {
+// //   gsap.to(".about", { duration: 2, x: 500 });
+// // });
+// // import barba from "@barba/core";
+// // import barbaCss from "@barba/css";
+
+// // // tell Barba to use the css plugin
+// // barba.use(barbaCss);
+
+// // // init Barba
+// // barba.init({
+// //   transition: [
+// //     {
+// //       once() {},
+// //     },
+// //   ],
+// // });
